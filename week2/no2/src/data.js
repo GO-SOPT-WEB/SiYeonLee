4052096b72621c149b2c70d1ec1c3ef5b7687913
SynthiaLee <seanne0801@gmail.com>
2023-04-18 16:07:30 +0900

    data tranferred to data JS file

---
+++ week2/no2/src/data.js
@@ -0,0 +1,74 @@
+export const initialCalender = [
+  {
+    day: "Mon",
+    date: "3",
+    things_to_do: "2",
+  },
+  {
+    day: "Tue",
+    date: "4",
+    things_to_do: "2",
+  },
+  {
+    day: "Wed",
+    date: "5",
+    things_to_do: "2",
+  },
+  {
+    day: "Thu",
+    date: "6",
+    things_to_do: "2",
+  },
+  {
+    day: "Fri",
+    date: "7",
+    things_to_do: "2",
+  },
+  {
+    day: "Sat",
+    date: "8",
+    things_to_do: "2",
+  },
+  {
+    day: "Sun",
+    date: "9",
+    things_to_do: "2",
+  },
+];
+
+export const initialCategories = [
+  {
+    category: "Everyday",
+    category: "Exercise",
+    category: "SOPT",
+    category: "Study",
+  },
+];
+
+export const initialEveryDayToDos = [
+  {
+    item: "가족식사🍽️",
+    item: "옷 정리🧦",
+  },
+];
+
+export const initialExerciseToDos = [
+  {
+    item: "스트레칭🧘🏼‍♀️",
+    item: "걷기🚶🏼‍♀️",
+  },
+];
+
+export const initialSoptToDos = [
+  {
+    item: "1주차 과제 완성✅",
+    item: "PR 날리기🚀",
+  },
+];
+
+export const initialStudyToDos = [
+  {
+    item: "코테 스터디 준비📝",
+    item: "리액트 강의 듣기💻",
+  },
+];
