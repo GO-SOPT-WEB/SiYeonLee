8b54c08a13e9b3a2399d3df022a40a8f3b63cde1
SynthiaLee <seanne0801@gmail.com>
2023-04-20 02:52:57 +0900

    selected category card display completed

--- week2/no1/src/pasta.js
+++ week2/no1/src/pasta.js
@@ -4,92 +4,101 @@ const menuArea = document.getElementById("menu_area");
 
 //메뉴파트
 
-menus.forEach((menu) => {
-  const card = document.createElement("article");
-
-  const title = document.createElement("h2");
-  title.innerText = menu.name;
-
-  const hashtagContainer = document.createElement("span");
-  hashtagContainer.className = "hashtag_container";
-
-  const hashtagList = document.createElement("ul");
-  hashtagList.className = "hashtag_list";
-
-  menu.hashtags.forEach((hashtag) => {
-    const hashtagItem = document.createElement("li");
-    hashtagItem.innerText = hashtag;
-    hashtagItem.className = "hashtag_item";
-
-    hashtagList.appendChild(hashtagItem);
+const displayMenus = () => {
+  menuArea.innerHTML = "";
+  const filteredMenus = [];
+  menus.forEach((menu) => {
+    if (categoryList.includes(menu.category) || categoryList.includes("all")) {
+      filteredMenus.push(menu);
+    }
   });
 
-  const plusIconWrapper = document.createElement("span");
-  plusIconWrapper.className = "plus_icon_button";
+  filteredMenus.forEach((menu) => {
+    const card = document.createElement("article");
 
-  plusIconWrapper.addEventListener("click", () => {
-    modal.style.display = "block";
+    const title = document.createElement("h2");
+    title.innerText = menu.name;
+
+    const hashtagContainer = document.createElement("span");
+    hashtagContainer.className = "hashtag_container";
+
+    const hashtagList = document.createElement("ul");
+    hashtagList.className = "hashtag_list";
+
+    menu.hashtags.forEach((hashtag) => {
+      const hashtagItem = document.createElement("li");
+      hashtagItem.innerText = hashtag;
+      hashtagItem.className = "hashtag_item";
+
+      hashtagList.appendChild(hashtagItem);
+    });
+
+    const plusIconWrapper = document.createElement("span");
+    plusIconWrapper.className = "plus_icon_button";
+
+    plusIconWrapper.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+    const plusIcon = document.createElement("i");
+    plusIcon.className = "fa-solid fa-plus";
+
+    const modal = document.createElement("div");
+    modal.className = "tag_modal";
+
+    const modalContent = document.createElement("div");
+    modalContent.className = "tag_modal_content";
+
+    const modalHashtagList = document.createElement("ul");
+    modalHashtagList.className = "modal_hashtag_list";
+
+    menu.hashtags.forEach((hashtag) => {
+      const modalHashtagItem = document.createElement("li");
+      modalHashtagItem.innerText = hashtag;
+      modalHashtagItem.className = "modal_hashtag_item";
+
+      modalHashtagList.appendChild(modalHashtagItem);
+    });
+
+    const closeIconWrapper = document.createElement("span");
+    closeIconWrapper.className = "close_icon_button";
+
+    closeIconWrapper.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    const closeIcon = document.createElement("i");
+    closeIcon.className = "fa-solid fa-xmark";
+
+    const image = document.createElement("img");
+    image.alt = menu.name;
+    image.src = menu.image_url;
+
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-heart";
+
+    plusIconWrapper.appendChild(plusIcon);
+
+    closeIconWrapper.appendChild(closeIcon);
+
+    modal.appendChild(modalContent);
+    modalContent.appendChild(modalHashtagList);
+    modalContent.appendChild(closeIconWrapper);
+    card.appendChild(modal);
+
+    card.appendChild(title);
+    card.appendChild(hashtagContainer);
+    hashtagContainer.appendChild(hashtagList);
+    hashtagContainer.appendChild(plusIconWrapper);
+    card.appendChild(image);
+    card.appendChild(icon);
+
+    menuArea.appendChild(card);
   });
-  const plusIcon = document.createElement("i");
-  plusIcon.className = "fa-solid fa-plus";
-
-  const modal = document.createElement("div");
-  modal.className = "tag_modal";
-
-  const modalContent = document.createElement("div");
-  modalContent.className = "tag_modal_content";
-
-  const modalHashtagList = document.createElement("ul");
-  modalHashtagList.className = "modal_hashtag_list";
-
-  menu.hashtags.forEach((hashtag) => {
-    const modalHashtagItem = document.createElement("li");
-    modalHashtagItem.innerText = hashtag;
-    modalHashtagItem.className = "modal_hashtag_item";
-
-    modalHashtagList.appendChild(modalHashtagItem);
-  });
-
-  const closeIconWrapper = document.createElement("span");
-  closeIconWrapper.className = "close_icon_button";
-
-  closeIconWrapper.addEventListener("click", () => {
-    modal.style.display = "none";
-  });
-
-  const closeIcon = document.createElement("i");
-  closeIcon.className = "fa-solid fa-xmark";
-
-  const image = document.createElement("img");
-  image.alt = menu.name;
-  image.src = menu.image_url;
-
-  const icon = document.createElement("i");
-  icon.className = "fa-solid fa-heart";
-
-  plusIconWrapper.appendChild(plusIcon);
-
-  closeIconWrapper.appendChild(closeIcon);
-
-  modal.appendChild(modalContent);
-  modalContent.appendChild(modalHashtagList);
-  modalContent.appendChild(closeIconWrapper);
-  card.appendChild(modal);
-
-  card.appendChild(title);
-  card.appendChild(hashtagContainer);
-  hashtagContainer.appendChild(hashtagList);
-  hashtagContainer.appendChild(plusIconWrapper);
-  card.appendChild(image);
-  card.appendChild(icon);
-
-  menuArea.appendChild(card);
-});
-
+};
 //카테고리 선택 파트
 
 const categoryButton = document.querySelectorAll("#type_list > label input");
-const categoryList = [];
+const categoryList = ["all"];
 
 categoryButton.forEach((button) => {
   button.addEventListener("click", (event) => {
@@ -101,6 +110,7 @@ categoryButton.forEach((button) => {
       }
     }
     displaySelectedCategory();
+    displayMenus();
   });
 });
 
@@ -147,4 +157,8 @@ const deleteSelectedCategory = (category) => {
   const index = categoryList.indexOf(category);
   categoryList.splice(index, 1);
   displaySelectedCategory();
+  displayMenus();
 };
+
+displayMenus();
+displaySelectedCategory();
